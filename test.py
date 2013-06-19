e75716ae9ba4bbf9427fb935122e0cee83c7111b
Allison Kaptur <allison.kaptur@gmail.com>
2013-06-19 12:13:53 -0400

    What we thought would be a failing test
    We were astonished that this test passed.  Then we realized that *that's* what that otherwise-mystifying multiplication by ten was doing.  Let's see if we can make this more clear.

--- test.py
+++ test.py
@@ -8,6 +8,12 @@ def test_whitespace():
     e = a.Evaluator()
     assert e.evaluate('2 * 3 + 1') == 7
 
+def test_multi_digit_num():
+    e = a.Evaluator()
+    assert e.evaluate('142*2') == 284
+
+
 if __name__ == '__main__':
     test_parser()
     test_whitespace()
+    test_multi_digit_num()
