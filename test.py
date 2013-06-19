4555e263670618474aee00f3328bdc53ecbbd299
Allison Kaptur <allison.kaptur@gmail.com>
2013-06-19 13:06:59 -0400

    new tests

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
 import algebra_parser as a
+import pdb
 
 def test_parser():
     e = a.Evaluator()
+    # pdb.set_trace()
     assert e.evaluate('2*3+1') == 7
+
+def test_multiply_at_end():
+    e = a.Evaluator()
+    assert e.evaluate('2+3*1') == 5
+
+def test_multiply_chained():
+    e = a.Evaluator()
+    assert e.evaluate('2+3*1*3') == 11
+
+def test_chained_precedence():
+    e = a.Evaluator()
+    assert e.evaluate('2+3*1*3+1') == 12
 
 def test_whitespace():
     e = a.Evaluator()
@@ -25,6 +39,9 @@ def test_tokenizer():
 
 if __name__ == '__main__':
     test_parser()
+    test_multiply_at_end()
+    test_multiply_chained()
+    test_chained_precedence()
     test_whitespace()
     test_multi_digit_num()
     test_tokenizer()
